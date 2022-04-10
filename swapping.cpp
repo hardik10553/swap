fe922572e284cc18dcdc2f079ada23b483be45b9
hardik10553 <70470976+hardik10553@users.noreply.github.com>
2022-04-11 00:09:01 +0530

    code file

---
+++ swapping.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+
+int p,q,temp;
+ p=4;
+ q=5
+temp=p;
+p=q;
+q=temp;
+cout<<"the value of p is"<<p<<endl;
+cout<<"the value of q is"<<q;
